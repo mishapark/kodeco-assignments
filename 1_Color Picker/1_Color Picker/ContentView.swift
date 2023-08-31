d79adb1939de0dec81a14ed1a101f47ccbc35edd
mishapark <packmisha@gmail.com>
2023-08-31 01:20:19 -0400

    done

--- 1_Color Picker/1_Color Picker/ContentView.swift
+++ 1_Color Picker/1_Color Picker/ContentView.swift
@@ -1,26 +1,61 @@
 //
 //  ContentView.swift
-//  1_Color Picker
+//  Color Picker
 //
-//  Created by Михаил Пак on 2023-08-31.
+//  Created by Mikhail Pak on 2023-08-30.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State private var redSliderValue: Double
+  @State private var greenSliderValue: Double
+  @State private var blueSliderValue: Double
+  @State private var color: Color
+
+  init(redSliderValue: Double = 0.0, greenSliderValue: Double = 255.0, blueSliderValue: Double = 0.0, color: Color = .white) {
+    self.redSliderValue = redSliderValue
+    self.greenSliderValue = greenSliderValue
+    self.blueSliderValue = blueSliderValue
+    self.color = Color(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue)
+  }
+
+  var body: some View {
+    VStack {
+      Text("Color Picker")
+        .font(.title)
+      RoundedRectangle(cornerRadius: 0)
+        .aspectRatio(contentMode: .fit)
+        .foregroundColor(color)
+      Text("Red")
+      HStack {
+        Slider(value: $redSliderValue, in: 0.0 ... 255.0)
+        Text("\(Int(redSliderValue.rounded()))")
+      }
+      Text("Green")
+      HStack {
+        Slider(value: $greenSliderValue, in: 0.0 ... 255.0)
+        Text("\(Int(greenSliderValue.rounded()))")
+      }
+      Text("Blue")
+      HStack {
+        Slider(value: $blueSliderValue, in: 0.0 ... 255.0)
+        Text("\(Int(blueSliderValue.rounded()))")
+      }
+      Button("Set Color") {
+        color = Color(
+          red: redSliderValue / 255,
+          green: greenSliderValue / 255,
+          blue: blueSliderValue / 255
+        )
+      }
     }
+    .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
