1ae47a1147286c491c9b7169d4b5b334d47b7b01
mishapark <packmisha@gmail.com>
2023-08-31 01:17:33 -0400

    first commit

---
+++ 1_Color Picker/1_Color Picker/__Color_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  __Color_PickerApp.swift
+//  1_Color Picker
+//
+//  Created by Михаил Пак on 2023-08-31.
+//
+
+import SwiftUI
+
+@main
+struct __Color_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
