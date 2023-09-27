f79c3590833863f18d774c310436b138110373f8
mishapark <packmisha@mail.ru>
2023-09-27 17:17:29 -0400

    created starter project

---
+++ Week 5/Task App/Task App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task App
+//
+//  Created by Mikhail Pak on 2023-09-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week 5/Task App/Task App/Task_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_AppApp.swift
+//  Task App
+//
+//  Created by Mikhail Pak on 2023-09-27.
+//
+
+import SwiftUI
+
+@main
+struct Task_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
