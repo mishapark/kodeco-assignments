ac48d04f5166f6d036acb8bf01f812ade01f4f1c
mishapark <packmisha@mail.ru>
2023-10-27 02:11:44 -0400

    Added new project

---
+++ Week 9/Week 9/Week 9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week 9
+//
+//  Created by Mikhail Pak on 2023-10-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week 9/Week 9/Week 9/Week_9App.swift
@@ -0,0 +1,17 @@
+//
+//  Week_9App.swift
+//  Week 9
+//
+//  Created by Mikhail Pak on 2023-10-27.
+//
+
+import SwiftUI
+
+@main
+struct Week_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
