80e4bbc8f6bcf2396b98d1ffc1b4e2a254ba6083
mishapark <packmisha@mail.ru>
2023-11-05 21:42:04 -0500

    done

---
+++ Week 8/Week 8/Week 8/Week_8App.swift
@@ -0,0 +1,17 @@
+//
+//  Week_8App.swift
+//  Week 8
+//
+//  Created by Mikhail Pak on 2023-11-05.
+//
+
+import SwiftUI
+
+@main
+struct Week_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
